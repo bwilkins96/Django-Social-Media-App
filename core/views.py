438f656eeca59b8b81fdb1394c319a127955ba72
bwilkins96 <ben.jr.speed@gmail.com>
2022-12-09 14:42:19 -0500

    user searching

--- core/views.py
+++ core/views.py
@@ -100,6 +100,34 @@ def follow(request):
         return redirect('/')
 
 @login_required(login_url='signin')
+def search(request):
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        username_objects = User.objects.filter(username__icontains=username)
+
+        username_profiles = []
+        username_profiles_list = []
+
+        for user in username_objects:
+            username_profiles.append(user.id)
+
+        for id in username_profiles:
+            profile_lists = Profile.objects.filter(id_user=id)
+            username_profiles_list.append(profile_lists)
+
+        username_profiles_list = list(chain(*username_profiles_list))
+    
+    context = {
+        'user_profile': user_profile, 
+        'username': username,
+        'username_profiles_list': username_profiles_list
+        }
+    return render(request, 'search.html', context)
+
+@login_required(login_url='signin')
 def upload(request):
     if request.method == 'POST':
         user = request.user.username
