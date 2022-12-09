65a2fb13e75536414f7aef73fc6f729df4533ab8
bwilkins96 <ben.jr.speed@gmail.com>
2022-12-09 11:37:39 -0500

    follows frontend

--- core/views.py
+++ core/views.py
@@ -21,12 +21,25 @@ def profile(request, pk):
     user_profile = Profile.objects.get(user=user_object)
     user_posts = Post.objects.filter(user=pk)
     user_posts_length = len(user_posts)
+    user_followers = len(FollowersCount.objects.filter(user=pk))
+    user_following = len(FollowersCount.objects.filter(follower=pk))
+
+    follower = request.user.username
+    user = pk
+
+    if FollowersCount.objects.filter(follower=follower, user=user).first():
+        button_text = 'Unfollow'
+    else:
+        button_text = 'Follow'
 
     context = {
         'user_object': user_object,
         'user_profile': user_profile,
         'user_posts': user_posts,
-        'user_posts_length': user_posts_length
+        'user_posts_length': user_posts_length,
+        'user_followers': user_followers,
+        'user_following': user_following,
+        'button_text': button_text
     }
     return render(request, 'profile.html', context)
 
